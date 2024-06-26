b0bb45d57b48d611195afb68cb245ade09ed2da6
GalliFrey7 <erkhanadenov@gmail.com>
2024-06-26 13:44:47 +0600

    second update for vercel

--- app/about-me/page.tsx
+++ app/about-me/page.tsx
@@ -10,7 +10,7 @@ const Page = () => {
         My Skills<span className="text-red-500">.</span>
         </h1>
         <p className="max-w-[350px] text-[16px] text-gray-200 md:text-gray-400">
-        My expertise encompasses web development, SEO optimization, and graphic design, all finely honed to meet the challenges of today's digital landscapes. I am committed to continual learning and mastery in my crafts, ensuring each project not only meets but exceeds expectations. Let’s collaborate to amplify your digital presence through innovative solutions and expert execution. Together, we can achieve exceptional results.
+        My expertise encompasses web development, SEO optimization, and graphic design, all finely honed to meet the challenges of modern digital landscapes. I am committed to continual learning and mastery in my crafts, ensuring each project not only meets but exceeds expectations. Go collaborate to amplify your digital presence through innovative solutions and expert execution. Together, we can achieve exceptional results.
         </p>
         </div>
         <div className="absolute -left-36 bottom-5 rotate-12 animate-pulse duration-75">
